241e9691cb3826e2820c02e4706bd4a1b84e1510
Alvaro <alvaro.fernandez@iesnervion.es>
2022-02-23 21:10:02 +0100

    Intentando arreglar el layout de los botones

--- src/app/services/pedidos.service.ts
+++ src/app/services/pedidos.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-import  { Observable } from 'rxjs'
+import { Observable } from 'rxjs'
 import { Pedido } from '../interfaces/pedido';
 
 
@@ -11,9 +11,12 @@ export class PedidosService {
 
   urlWebApi = "https://crudpersonasasp.azurewebsites.net/api/Persona";
 
-  constructor(private http: HttpClient) {  }
+  constructor(private http: HttpClient) { }
 
-  listadoPedidos(): Observable<Pedido[]> { return this.http.get<Pedido[]>(this.urlWebApi); }  
-  pedidoPorId(id: number): Observable<Pedido>{ return this.http.get<Pedido>(this.urlWebApi+"/"+id)}
-  
+  listadoPedidos(): Observable<Pedido[]> { return this.http.get<Pedido[]>(this.urlWebApi); }
+  pedidoPorId(id: number): Observable<Pedido> { return this.http.get<Pedido>(this.urlWebApi + "/" + id) }
+  //Esto va en otro service o es en este mismo?
+  insertPedido(pedido: Pedido): void {
+    this.http.post<Pedido>(this.urlWebApi, pedido);
+  }
 }
