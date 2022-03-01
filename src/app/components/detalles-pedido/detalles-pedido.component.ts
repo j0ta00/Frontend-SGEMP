9d53e7bd920e4ea60dbcb67e6afb22f7565f5197
Alvaro <alvaro.fernandez@iesnervion.es>
2022-03-01 18:00:24 +0100

    Binding de las fechas

--- src/app/components/detalles-pedido/detalles-pedido.component.ts
+++ src/app/components/detalles-pedido/detalles-pedido.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChange } from '@angular/core';
 import { PedidosService } from 'src/app/services/pedidos.service';
 import { Pedido } from 'src/app/interfaces/pedido';
 import { ActivatedRoute, Route, Router } from '@angular/router';
@@ -8,19 +8,22 @@ import { ActivatedRoute, Route, Router } from '@angular/router';
   styleUrls: ['./detalles-pedido.component.css']
 })
 export class DetallesPedidoComponent implements OnInit {
-  pedidoID:number;
+  pedidoID: number;
   pedidoSeleccionado: Pedido;
 
-  constructor(private pedidosService: PedidosService, private activatedRoute: ActivatedRoute, private router:Router) {}
+  constructor(private pedidosService: PedidosService, private activatedRoute: ActivatedRoute, private router: Router) { }
+  // ngOnChanges(changes:SimpleChange):void {
+  //   if(typeof changes['Pedido']!=="undefined")
+  //   var change=changes['pedidoSeleccionado'];
+  //   if(!change.isFisrtChange())
+  //   this.cargarPedido();
+  // }
   ngOnInit(): void {
-    this.pedidoID=Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    this.pedidoID = Number(this.activatedRoute.snapshot.paramMap.get('id'));
     this.cargarPedido();
   }
-  ngAfterViewInit():void {
-    console.log(this.pedidoSeleccionado.id)
-  }
   guardarCambios(): void {
-    alert(this.pedidoSeleccionado.id)
+    //llamar a la API para actualizar los datos del pedido
   }
   descartarCambios(): void {
     this.router.navigateByUrl("/home");
@@ -28,17 +31,14 @@ export class DetallesPedidoComponent implements OnInit {
   crearPedido(): void {
     this.router.navigateByUrl("/create");
   }
-  loadPedidosasync():void{
-
-  }
-  cargarPedido():void{
-    this.pedidosService.pedidoPorId(this.pedidoID).subscribe(data=>this.pedidoSeleccionado={
-      id:data.id,
-      fechaPedido:data.fechaPedido,
-      fechaRecepcion:data.fechaRecepcion,
-      importeTotal:data.importeTotal,
-      esBorrado:data.esBorrado,
-      idProveedor:data.idProveedor 
+  cargarPedido(): void {
+    this.pedidosService.pedidoPorId(this.pedidoID).subscribe(data => this.pedidoSeleccionado = {
+      id: data.id,
+      fechaPedido: data.fechaPedido,
+      fechaRecepcion: data.fechaRecepcion,
+      importeTotal: data.importeTotal,
+      esBorrado: data.esBorrado,
+      idProveedor: data.idProveedor
     });
   }
 
