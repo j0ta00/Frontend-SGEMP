edba81c5fbbde95ac589659de09c20e021863018
j0ta00 <rypperx@gmail.com>
2022-03-01 21:53:21 +0100

    Botones de borrado,de añadir filas añadidos y de editar, hay que unir el componente con el de Álvaro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,6 @@ import { AuthService } from './services/auth.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+ 
   
 }
--- src/app/components/listado-lineas-pedidos/listado-lineas-pedidos.component.ts
+++ src/app/components/listado-lineas-pedidos/listado-lineas-pedidos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import{LineaPedidos} from 'src/app/interfaces/linea-pedidos'
-import {LineasPedidosService} from 'src/app/services/lineas-pedidos.service';
+import { LineaPedidos } from 'src/app/interfaces/linea-pedidos'
+import { LineasPedidosService } from 'src/app/services/lineas-pedidos.service';
+
 
 @Component({
   selector: 'app-listado-lineas-pedidos',
@@ -8,18 +9,44 @@ import {LineasPedidosService} from 'src/app/services/lineas-pedidos.service';
   styleUrls: ['./listado-lineas-pedidos.component.css']
 })
 export class ListadoLineasPedidosComponent implements OnInit {
-listadoLineasPedidos:LineaPedidos[];
+  listadoLineasPedidos: LineaPedidos[];
+  listadoLineasPedidosAnhiadidos: LineaPedidos[];
 
+  constructor(private LineasPedidosService: LineasPedidosService) {
+    this.anhiadiendoPedido=false
+    this.visibilityTd = false;
+    this.visibilityInputs = true;
+  }
+  private nuevoPedido: LineaPedidos;
+  public visibilityTd: boolean;
+  public visibilityInputs: boolean;
+  public anhiadiendoPedido: boolean;
+  public cambiarVisibilidad() {
+    this.visibilityTd = true;
+    this.visibilityInputs = false;
+  }
+  public anhiadirPedido(){
+    this.anhiadiendoPedido=true; 
+    this.listadoLineasPedidosAnhiadidos=this.listadoLineasPedidosAnhiadidos==null?new Array():this.listadoLineasPedidosAnhiadidos;
+    this.listadoLineasPedidosAnhiadidos.push(this.nuevoPedido);
+    this.nuevoPedido={
+     cantidad:0,
+     subTotal:0,
+     idProducto:'',
+     idPedido:'',
+     precioUnitario:0
+      
 
-  constructor(private LineasPedidosService: LineasPedidosService){ 
+      
+    };
 
   }
+  public borrarFila(fila: number) { }
 
   ngOnInit(): void {
-
-this.LineasPedidosService.listadoLineaPedidos(1).subscribe(data=>{
-  this.listadoLineasPedidos=data
-});
+    this.LineasPedidosService.listadoLineaPedidos(1).subscribe(data => {
+      this.listadoLineasPedidos = data
+    });
 
   }
 
