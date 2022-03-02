a1c6bf09c7df945ba935bd2c0269eefc87e4b96a
Alvaro <alvaro.fernandez@iesnervion.es>
2022-03-02 19:55:43 +0100

    Aun no se usar el resolver

---
+++ src/app/resolvers/pedidos.resolver.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {
+  Router, Resolve,
+  RouterStateSnapshot,
+  ActivatedRouteSnapshot
+} from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { Pedido } from '../interfaces/pedido';
+import { PedidosService } from '../services/pedidos.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PedidosResolver implements Resolve<Pedido> {
+  constructor(private pedidosService:PedidosService){}
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Pedido> {
+    return this.pedidosService.pedidoPorId(Number(route.paramMap.get("id")))
+  }
+}
