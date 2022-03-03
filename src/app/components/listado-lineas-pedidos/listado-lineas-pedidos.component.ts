a3457e5b5f525ae76a107814aa52faacfb74dbae
Ángel López <angel.lopez@iesnervion.es>
2022-03-03 14:20:31 +0100

    Detalles Pedidos Con Base Imponible IVA y Total

--- src/app/components/listado-lineas-pedidos/listado-lineas-pedidos.component.ts
+++ src/app/components/listado-lineas-pedidos/listado-lineas-pedidos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import{LineaPedidos} from 'src/app/interfaces/linea-pedidos'
-import {LineasPedidosService} from 'src/app/services/lineas-pedidos.service';
+import { LineaPedidos } from 'src/app/interfaces/linea-pedidos'
+import { LineasPedidosService } from 'src/app/services/lineas-pedidos.service';
+import { convertToObject } from 'typescript';
 
 @Component({
   selector: 'app-listado-lineas-pedidos',
@@ -8,19 +9,48 @@ import {LineasPedidosService} from 'src/app/services/lineas-pedidos.service';
   styleUrls: ['./listado-lineas-pedidos.component.css']
 })
 export class ListadoLineasPedidosComponent implements OnInit {
-listadoLineasPedidos:LineaPedidos[];
+  listadoLineasPedidos: LineaPedidos[];
+  baseImponible: Number;
+  iva: Number;
+  total: Number;
 
 
-  constructor(private LineasPedidosService: LineasPedidosService){ 
-
+  constructor(private LineasPedidosService: LineasPedidosService) {
+    this.baseImponible = 0;
+    this.iva = 0;
+    this.total = 0;
   }
 
   ngOnInit(): void {
 
-this.LineasPedidosService.listadoLineaPedidos(1).subscribe(data=>{
-  this.listadoLineasPedidos=data
-});
-
+    this.LineasPedidosService.listadoLineaPedidos(1).subscribe(data => {
+      this.listadoLineasPedidos = data
+    });
+    
   }
 
+  click(): void {
+    this.baseImponible = this.calcularBaseImponible();
+    this.iva = this.calcularIva();
+    this.total = this.calcularTotal();
+    console.log(this.baseImponible);
+  }
+
+  calcularBaseImponible(): Number {
+    var base = 0;
+    for (let linea of this.listadoLineasPedidos) {
+      base += Number(linea.subTotal);
+    }
+    return base;
+  }
+
+  calcularIva(): Number {
+    
+    return Number(this.baseImponible) * 0.21;
+  }
+
+  calcularTotal(): Number{
+    return Number(this.baseImponible) + Number(this.iva);
+  }
 }
+
